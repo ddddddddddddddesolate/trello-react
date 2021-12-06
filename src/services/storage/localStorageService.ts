2cad3fed6930a14345b6951c6b2e2c8bc61d881b
Alexander <aleksandr.kalashnik@purrweb.com>
2021-12-06 10:45:00 +0600

    implemented localStorageService

---
+++ src/services/storage/localStorageService.ts
@@ -0,0 +1,14 @@
+const localStorageService = {
+  getItem: (key: string): any => {
+    const item = localStorage.getItem(key);
+
+    if (item === null) return;
+
+    return JSON.parse(item);
+  },
+  setItem: (key: string, value: any): void => {
+    return localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+export default localStorageService;
