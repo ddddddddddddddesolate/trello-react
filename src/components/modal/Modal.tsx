87604a1100ca73d8f0a86ae66beb67a8a420a3fb
Alexander <aleksandr.kalashnik@purrweb.com>
2021-12-15 17:11:06 +0600

    implemented modal component

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,79 @@
+import { FC, ReactElement, useCallback } from 'react';
+import { createPortal } from 'react-dom';
+import styled from 'styled-components';
+import closeIcon from '../../assets/icons/close.svg';
+
+interface PortalProps {
+  children: ReactElement,
+}
+
+interface ModalProps {
+  visible: boolean,
+  close: () => void,
+  children: ReactElement,
+}
+
+const Portal:FC<PortalProps> = ({ children }) =>
+  createPortal(children, document.body);
+
+const Modal:FC<ModalProps> = ({ visible, close, children }) => {
+  const handleCloseModal = useCallback(() => {
+    close();
+  }, []);
+
+  return (
+    <Portal>
+      <>
+        {visible && (
+          <Container>
+            <Content>
+              <CloseButton onClick={handleCloseModal} />
+              {children}
+            </Content>
+          </Container>
+        )}
+      </>
+    </Portal>
+  );
+};
+
+const Container = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, .3);
+  overflow: hidden;
+`;
+
+const Content = styled.div`
+  width: 30%;
+  margin: 20% auto;
+  height: fit-content;
+  background: #fefefe;
+  padding: 30px;
+  border-radius: 5px;
+  position: relative;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: url(${closeIcon}) no-repeat center;
+  background-size: 15px 15px;
+  padding: 10px;
+  width: 30px;
+  height: 30px;
+  z-index: 2;
+  border: none;
+  cursor: pointer;
+  transition: .3s;
+  
+  &:hover {
+    opacity: .5;
+  }
+`;
+
+export default Modal;
