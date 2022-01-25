cbd2cf3667289b09121fca32e19a5795411b83fd
Alexander <aleksandr.kalashnik@purrweb.com>
2022-01-25 22:47:38 +0600

    add cards to column

--- src/components/column/Column.tsx
+++ src/components/column/Column.tsx
@@ -1,29 +1,112 @@
-import { FC } from 'react';
+import { FC, useCallback, useEffect, useState } from 'react';
 
 import styled from 'styled-components';
+import { v4 as uuid } from 'uuid';
+
+import localStorageService from 'services/storage';
 
 import ColumnNameForm from './components/columnNameForm';
+import AddCardButton from './components/addCardButton';
+import CardsList from './components/cardsList';
+
+import { CardType } from 'types/cardType';
+import AddCardForm from 'components/column/components/addCardForm';
 
 interface ColumnProps {
-  id: number,
-  name: string,
+  id: string;
+  name: string;
 }
 
-const Column:FC<ColumnProps> = ({ id, name }) => {
-  return(
+const Column: FC<ColumnProps> = ({ id, name }) => {
+  const [createCardFormVisible, setCreateCardFormVisible] =
+    useState<boolean>(false);
+  const [cards, setCards] = useState<CardType[]>([]);
+
+  const getCards = useCallback((): CardType[] => {
+    const allCards = localStorageService.getItem('cards') || [];
+
+    return allCards.filter((card: CardType) => card.columnId === id);
+  }, []);
+
+  const handleOpenCreateCardForm = useCallback(
+    (): void => setCreateCardFormVisible(true),
+    []
+  );
+
+  const handleCreateCard = useCallback(
+    (name: string): void => {
+      let cards = localStorageService.getItem('cards') || [];
+
+      cards.unshift({
+        id: uuid(),
+        columnId: id,
+        name: name,
+      });
+
+      localStorageService.setItem('cards', cards);
+
+      setCards(getCards());
+      setCreateCardFormVisible(false);
+    },
+    [getCards, id]
+  );
+
+  useEffect(() => setCards(getCards()), []);
+
+  return (
     <StyledContainer>
       <ColumnNameForm id={id} name={name} />
+
+      <StyledCardsContainer>
+        {createCardFormVisible && <AddCardForm onSubmit={handleCreateCard} />}
+
+        {cards.length !== 0 ? (
+          <CardsList cards={cards} />
+        ) : (
+          <StyledEmpty>No cards found</StyledEmpty>
+        )}
+
+        <AddCardButton onClick={handleOpenCreateCardForm} />
+      </StyledCardsContainer>
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.div`
   background-color: #dcdde1;
+
   border-radius: 4px;
   width: 280px;
   height: fit-content;
   margin-left: 12px;
   padding: 10px;
+`;
+
+const StyledCardsContainer = styled.div`
+  height: fit-content;
+  width: 270px;
+  padding: 5px;
+
+  border-radius: 3px;
+
+  font-size: 18px;
+  line-height: 18px;
+
+  background-color: rgba(0, 0, 0, 0.03);
+  color: rgba(0, 0, 0, 0.6);
+
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+
+  gap: 10px;
+`;
+
+const StyledEmpty = styled.div`
+  width: 100%;
+  padding: 10px 0;
+
+  text-align: center;
 `;
 
 export default Column;
